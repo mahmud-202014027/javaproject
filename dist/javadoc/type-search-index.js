0f966cba362c54bcaec71fa8594c42e5acbb9b9f
mahmud-202014027 <mahmudhasan8832@gmail.com>
2021-12-27 14:58:02 +0600

    first commit

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"money.manager","l":"home"},{"p":"money.manager","l":"login"},{"p":"money.manager","l":"MoneyManager"}];updateSearchResults();
